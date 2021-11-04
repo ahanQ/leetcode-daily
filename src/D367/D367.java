8e20199108c461ce2d80ce130925114c59ebec6f
luliuquan <luliuquan@weatherone.com.cn>
2021-11-04 10:37:17 +0800

    2021年11月4日 改版

--- src/D367/D367.java
+++ src/D367/D367.java
@@ -8,19 +8,22 @@ public class D367 {
     public static void main(String[] args) {
         System.out.println(new Solution().isPerfectSquare(16));
         System.out.println(new Solution().isPerfectSquare(14));
+        System.out.println(new Solution().isPerfectSquare(808201));
     }
 }
 class Solution {
     public boolean isPerfectSquare(int num) {
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            final int x = i * i;
-            if (x == num) {
+        int l = 1, r = Math.min(num / 2, 46340);
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (m * m < num) {
+                l = m + 1;
+            } else if (m * m > num) {
+                r = m - 1;
+            } else {
                 return true;
             }
-            if (x > num) {
-                return false;
-            }
         }
-        return false;
+        return l * l == num;
     }
 }
